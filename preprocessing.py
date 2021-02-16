a913e257b9a609ea034ef8eee7f17ada422544b0
rohan.kirpy <rohan.kirpy@gmail.com>
2021-02-16 16:04:52 +1100

    Added historical data files of major leagues

---
+++ preprocessing.py
@@ -0,0 +1,23 @@
+
+'''
+This code is used to perform the following tasks:
+1. Preprocess data for a list of teams
+'''
+
+# Import libraries
+import os
+import pandas as pd
+import numpy as np
+
+
+# File location
+folder_name = "./data"
+files = os.listdir(folder_name)
+
+data_file = "./data/E0.csv"
+
+
+data = pd.read_csv(data_file)
+
+list_of_teams = 
+
